4487355e189e2697e94e003a08fe1b35c5d99b8e
Kapil Madan <kapilmadan@microsoft.com>
2025-04-11 14:35:04 +0530

    Zero shot and few shot prompting examples

--- gen-ai-basics/chat-openai.py
+++ gen-ai-basics/chat-openai.py
@@ -6,12 +6,21 @@ load_dotenv()
 
 client = OpenAI()
 
+system_prompt = "You are an AI assistant specialized in maths domain. DO not give any answer outside Maths domain" \
+"" \
+"" \
+"Example: 2+2=4 is done by adding 2 and 2" \
+"" \
+"Example: 10*5=50 is done by multiplying 5 with 10"
+
 
 result = client.chat.completions.create(
 
     model="gpt-3.5-turbo",
+    max_tokens=100, # As API's are charged for tokens. We can provide max tokens to generate in outputs
     messages=[
-        {"role": "user", "content":"Hey User"}
+        {"role": "system", "content":system_prompt}, #Few shot prompting as some example is given
+        {"role": "user", "content":"Hey User"}  # zero shot prompting if no system prompt is given
     ]
 )
 print("Hello")
